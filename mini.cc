c063f12fdf326744385ec3d1ec3bb177074383e8
Max Neunhoeffer <max@arangodb.com>
2019-01-06 21:45:18 +0100

    Standalone client and server as test.

---
+++ mini.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+#include <nghttp2/asio_http2_server.h>
+
+using namespace nghttp2::asio_http2;
+using namespace nghttp2::asio_http2::server;
+
+int main(int argc, char *argv[]) {
+  try {
+    // Check command line arguments.
+    if (argc < 4) {
+      std::cerr
+          << "Usage: mini <address> <port> <threads> [<private-key-file> "
+          << "<cert-file>]\n";
+      return 1;
+    }
+
+    boost::system::error_code ec;
+
+    std::string addr = argv[1];
+    std::string port = argv[2];
+    std::size_t num_threads = std::stoi(argv[3]);
+
+    http2 server;
+
+    server.num_threads(num_threads);
+
+    server.handle("/hello", [](const request &req, const response &res) {
+      res.write_head(200, {{"x-mini-info", {"hicks"}}});
+      res.end("hello, world\n");
+    });
+
+    if (argc >= 6) {
+      boost::asio::ssl::context tls(boost::asio::ssl::context::sslv23);
+      tls.use_private_key_file(argv[4], boost::asio::ssl::context::pem);
+      tls.use_certificate_chain_file(argv[5]);
+
+      configure_tls_context_easy(ec, tls);
+
+      if (server.listen_and_serve(ec, tls, addr, port)) {
+        std::cerr << "error: " << ec.message() << std::endl;
+      }
+    } else {
+      if (server.listen_and_serve(ec, addr, port)) {
+        std::cerr << "error: " << ec.message() << std::endl;
+      }
+    }
+  } catch (std::exception &e) {
+    std::cerr << "exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
